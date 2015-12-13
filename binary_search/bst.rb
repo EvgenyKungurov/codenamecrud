044b5fd3f29dd4cae07b7ff8bb72875fc90099f3
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2015-12-13 19:03:18 +0800

    Add recursion

--- binary_search/bst.rb
+++ binary_search/bst.rb
@@ -10,9 +10,43 @@ class BTS
   def build_tree(arr)
     @root = Node.new(arr.shift) if @root.value == nil
     arr.each { |v| @root.insert(v) }
-    @root.inspect
   end
+
+  def breadth_first_search(v)
+    queue = [root]
+    until queue.empty?
+      node = queue.shift
+      return node.inspect if node.value == v
+      queue.push node.left_child unless node.left_child.nil?
+      queue.push node.right_child unless node.right_child.nil?
+    end
+    nil
+  end
+
+  def depth_first_search(v)
+    stack = [root]
+    until stack.empty?
+      node = stack.pop
+      return node.inspect if node.value == v
+      stack.push node.left_child unless node.left_child.nil?
+      stack.push node.right_child unless node.right_child.nil?
+    end
+    nil
+  end
+
+  def dfs_rec(v, current_node = root)
+    return current_node if current_node.value == v
+    result = dfs_rec(v, current_node.left_child) unless current_node.left_child.nil?
+    return result if result
+    result = dfs_rec(v, current_node.right_child) unless current_node.right_child.nil?
+    return result if result
+    nil
+  end
+
 end
 
 bts = BTS.new(1000)
-puts bts.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+bts.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+#puts bts.breadth_first_search(9)
+#puts bts.depth_first_search(9)
+puts bts.dfs_rec(9)
--- binary_search/node.rb
+++ binary_search/node.rb
@@ -1,12 +1,12 @@
 class Node
   attr_reader :value
-  attr_accessor :parent, :left_child, :right_child
+  attr_accessor :parent_id, :left_child, :right_child
 
   def initialize(value, parent = nil, left_child = nil, right_child = nil)
     @value = value
     @left_child = left_child
     @right_child = right_child
-    @parent = parent
+    @parent_id = parent
   end
 
   def insert(v)
@@ -16,10 +16,10 @@ class Node
     when  0 then false
     end
   end
-
-  def inspect
-    "Left: #{left_child.inspect} Node: #{value} Right: #{right_child.inspect}"
-  end
+  #
+  # def inspect
+  #   "Left: #{left_child.inspect} Node: #{value} Right: #{right_child.inspect}"
+  # end
 
   private
 
@@ -27,7 +27,7 @@ class Node
     if left_child
       left_child.insert(v)
     else
-      self.left_child = Node.new(v, self)
+      self.left_child = Node.new(v, self.object_id)
     end
   end
 
@@ -35,7 +35,7 @@ class Node
     if right_child
       right_child.insert(v)
     else
-      self.right_child = Node.new(v, self)
+      self.right_child = Node.new(v, self.object_id)
     end
   end
 end
